829acce6ceb9c93b73d730f6d8aca475e4e80020
TAYOWORLD <124199388+TAYOWORLD@users.noreply.github.com>
2023-02-15 14:14:47 -0500

    Shape will expand and change color
    The black and white abstract shape will become bigger due to the location of the mouse and a part of the shape will change colors as well.

---
+++ mouseXlabwork.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(800,800);
+}
+
+function draw() {
+  background(220);
+  
+if (mouseX > 100){
+  scale(2);
+} else if (mouseY > 300){
+  fill("gold"); 
+}else mouseX >= 100 && mouseX < 200
+  scale(2);
+
+  
+  rect(50,50,100)
+  fill("white")
+  ellipse(80,80,100)
+  fill ("black")
+  ellipse(80,80,40)
+   
+  
+}
+
